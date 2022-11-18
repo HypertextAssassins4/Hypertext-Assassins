3dcb092feb265e971b4816e820bfa020a21d3ca3
imarcus14 <114891075+imarcus14@users.noreply.github.com>
2022-11-17 20:59:02 -0500

    Update diary.js

--- diary.js
+++ diary.js
@@ -6,6 +6,30 @@ function makePassword(){
     alert("The passwords do not match")
   }
  }
+function hunain(){
+  var username = username
+var password = password
+(function promptPass() {
+
+var p1 = prompt("Enter username");
+var p2 = prompt("Enter Password");
+
+  while (p1 !== username) {
+    alert("Incorrect Username");
+    return promptPass();
+  }
+  
+  
+  while (p2 !== password) {
+    alert("Incorrect Password");
+    return promptPass();
+  }
+
+}());
+}
+
+
+alert('welcome');
 function unlock(){
   var unlockUsername=prompt("What is your username?")
   var ulock=prompt("What is the password?")
