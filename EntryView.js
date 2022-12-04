e6c2ab402c3c8691f85f330abef77661858a8e1f
Hunainsh <114962972+Hunainsh@users.noreply.github.com>
2022-12-04 15:56:20 -0500

    Add files via upload

---
+++ EntryView.js
@@ -0,0 +1,97 @@
+export default class EntryView {
+    constructor(root, { onEntrySelect, onEntryAdd, onEntryEdit, onEntryDelete } = {}) {
+        this.root = root;
+        this.onEntrySelect = onEntrySelect;
+        this.onEntryAdd = onEntryAdd;
+        this.onEntryEdit = onEntryEdit;
+        this.onEntryDelete = onEntryDelete;
+        this.root.innerHTML = `
+            <div class="entry__sidebar">
+                <button class="entry__add" type="button">Add Entry</button>
+                <div class="entry__list"></div>
+            </div>
+            <div class="entry__preview">
+                <input class="entry__title" type="text" placeholder="New Entry...">
+                <textarea class="entry__body">Take Entry...</textarea>
+            </div>
+        `;
+
+        const btnAddEntry = this.root.querySelector(".entry__add");
+        const inpTitle = this.root.querySelector(".entry__title");
+        const inpBody = this.root.querySelector(".entry__body");
+
+        btnAddEntry.addEventListener("click", () => {
+            this.onEntryAdd();
+        });
+
+        [inpTitle, inpBody].forEach(inputField => {
+            inputField.addEventListener("blur", () => {
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value.trim();
+
+                this.onEntryEdit(updatedTitle, updatedBody);
+            });
+        });
+
+        this.updateEntryPreviewVisibility(false);
+    }
+
+    _createListItemHTML(id, title, body, updated) {
+        const MAX_BODY_LENGTH = 60;
+
+        return `
+            <div class="entry__list-item" data-entry-id="${id}">
+                <div class="entry__small-title">${title}</div>
+                <div class="entry__small-body">
+                    ${body.substring(0, MAX_BODY_LENGTH)}
+                    ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+                </div>
+                <div class="entry__small-updated">
+                    ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                </div>
+            </div>
+        `;
+    }
+
+    updateEntryList(entries) {
+        const entryListContainer = this.root.querySelector(".entry__list");
+
+        entryListContainer.innerHTML = "";
+
+        for (const entry of entries) {
+            const html = this._createListItemHTML(entry.id, entry.title, entry.body, new Date(entry.updated));
+
+            entryListContainer.insertAdjacentHTML("beforeend", html);
+        }
+
+        entryListContainer.querySelectorAll(".entry__list-item").forEach(entryListItem => {
+            entryListItem.addEventListener("click", () => {
+                this.onEntrySelect(entryListItem.dataset.entryId);
+            });
+
+            entryListItem.addEventListener("dblclick", () => {
+                const doDelete = confirm("Are you sure you want to delete this entry?");
+
+                if (doDelete) {
+                    this.onEntryDelete(entryListItem.dataset.entryId);
+                }
+            });
+        });
+    }
+
+    updateActiveEntry(entry) {
+        this.root.querySelector(".entry__title").value = entry.title;
+        this.root.querySelector(".entry__body").value = entry.body;
+
+        this.root.querySelectorAll(".entry__list-item").forEach(entryListItem => {
+            entryListItem.classList.remove("entry__list-item--selected");
+        });
+
+        console.log(entry.id)
+        this.root.querySelector(`.entry__list-item[data-entry-id="${entry.id}"]`).classList.add("entry__list-item--selected");
+    }
+
+    updateEntryPreviewVisibility(visible) {
+        this.root.querySelector(".entry__preview").style.visibility = visible ? "visible" : "hidden";
+    }
+}
---
+++ app.js
@@ -0,0 +1,64 @@
+import EntryView from "./EntryView.js";
+import EntryAPI from "./entryapi.js";
+
+export default class App {
+    constructor(root) {
+        this.entry = [];
+        this.activeEntry = null;
+        this.view = new EntryView(root, this._handlers());
+
+        this._refreshEntry();
+    }
+
+    _refreshEntry() {
+        const entry = EntryAPI.getAllEntries();
+
+        this._setEntry(entry);
+
+        if (entry.length > 0) {
+            this._setActiveEntry(entry[0]);
+        }
+    }
+
+    _setEntry(entry) {
+        this.entry = entry;
+        this.view.updateEntryList(entry);
+        this.view.updateEntryPreviewVisibility(entry.length > 0);
+    }
+
+    _setActiveEntry(entry) {
+        this.activeEntry = entry;
+        this.view.updateActiveEntry(entry);
+    }
+
+    _handlers() {
+        return {
+            onEntrySelect: entryId => {
+                const selectedEntry = this.entry.find(entry => entry.id == entryId);
+                this._setActiveEntry(selectedEntry);
+            },
+            onEntryAdd: () => {
+                const newEntry = {
+                    title: "New Entry",
+                    body: "Take Entry..."
+                };
+
+                EntryAPI.saveEntry(newEntry);
+                this._refreshEntry();
+            },
+            onEntryEdit: (title, body) => {
+                EntryAPI.saveEntry({
+                    id: this.activeEntry.id,
+                    title,
+                    body
+                });
+
+                this._refreshEntry();
+            },
+            onEntryDelete: entryId => {
+                EntryAPI.deleteEntry(entryId);
+                this._refreshEntry();
+            },
+        };
+    }
+}
