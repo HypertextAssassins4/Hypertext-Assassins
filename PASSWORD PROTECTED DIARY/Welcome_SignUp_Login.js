87eb1313fabbc3db461b1ec82246e0cfe8637fa8
imarcus14 <114891075+imarcus14@users.noreply.github.com>
2022-12-04 23:58:23 -0500

    Update Welcome_SignUp_Login.js

--- PASSWORD PROTECTED DIARY/Welcome_SignUp_Login.js
+++ PASSWORD PROTECTED DIARY/Welcome_SignUp_Login.js
@@ -1,15 +1,19 @@
+//global variables
 var username, password, validInput;
 document.getElementById("errMsg").style.color="red";
 
 function storeInfo() {
-    username = document.getElementById("usrname").value;
-    password = document.getElementById("pswrd").value;
+    //Retrieving username and password from local storage
+    username = localStorage.getItem("user");
+    password = localStorage.getItem("sCode");
     confirmLogin(username, password);
 }
-
+//Function to check if username is valid
 function checkUsername() {
     var valid = false;
     username = document.getElementById("usrname").value;
+    //Setting username in local storage so that it can compared later for login
+    localStorage.setItem("user", username);
     if (username.length > 0 && username.length < 6) {
         document.getElementById("errMsg").innerHTML="Your username must be at least 6 characters long";
         return valid;
@@ -18,9 +22,12 @@ function checkUsername() {
         return valid;
     }
 }
+//Function to check if password is valid
 function checkPassword() {
     document.getElementById("errMsg").innerHTML="";
     password = document.getElementById("pswrd").value;
+    //Setting password in local storage so that it can compared later for login
+    localStorage.setItem("sCode", password);
     var valid=false;
     if (password.length < 8) {
         document.getElementById("errMsg").innerHTML="Your password must be at least 8 characters long";
@@ -30,6 +37,7 @@ function checkPassword() {
         return valid;
     }
 }
+//Function to check if both password entries are the same
 function confirmPassword() {
     var temp = document.getElementById("pswrdRe").value;
     var confirm = false;
@@ -40,7 +48,7 @@ function confirmPassword() {
     }
     signUp(confirm);
 }
-
+//Function to inform user if sign up is successful or not
 function signUp (confirm) {
     document.getElementById("useless")==false;
     if (checkUsername()==true && checkPassword()==true && confirm==true) {
@@ -52,7 +60,7 @@ function signUp (confirm) {
     }
 }
 
-
+//Function to inform user if login is successful or not
 function confirmLogin(user, secrtCode) {
     var correctInput = false;
     var check1 = document.getElementById("urname").value;
@@ -66,7 +74,7 @@ function confirmLogin(user, secrtCode) {
     }
    logIn(correctInput);
 }
-
+//Function to redirect user to diary catalog page if login is successful
 function logIn(correctInput) {
     if (correctInput==true){
         validInput = document.getElementById("submit").onclick=mainPage();
@@ -76,8 +84,7 @@ function logIn(correctInput) {
         alert("Please try again!");
     }
 }
-
+//Redirects user to diary catalog page if login or sign up is successful
 function mainPage() {
     location.href="./index.html";
 }
-
